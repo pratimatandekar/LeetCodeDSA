61bb57195b6f6fc2eb2920d8cfcbf0593951ab63
Pratima_Tandekar <116260245+pratimatandekar@users.noreply.github.com>
2024-05-15 21:07:08 +0530

    Add files via upload

---
+++ Sample/Sample.java
@@ -0,0 +1,9 @@
+package Sample;
+
+import Demo.Demo;
+
+public class Sample extends Demo {
+    public static void main(String[] args) {
+      //  System.out.println(printReverse("user"));
+    }
+}
